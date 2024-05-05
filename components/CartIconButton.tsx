3fa70f484e8a2d4b696eada200034acfc4d52e5b
Rodrigo Torres <32571199+rodrigotorres28@users.noreply.github.com>
2024-05-05 04:53:24 -0300

    Alert para el emptyCart

--- components/CartIconButton.tsx
+++ components/CartIconButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../state/store';
 import { emptyCart } from '../state/cartSlice';
@@ -10,12 +10,17 @@ const CartIconButton = (props: CartIconProps) => {
   const dispatch = useDispatch();
   const cartItems = useSelector((state: RootState)  => state.cart.items);
 
+  const emptyCartAlert = () =>
+    Alert.alert('Emptying Cart', "Are you sure you want to remove all items from the cart?" , [
+      {
+        text: 'Cancel',
+      },
+      {text: 'OK', onPress: () => dispatch(emptyCart())},
+    ]);
+
   return (
     <TouchableOpacity
-    
-      onPress={() => {
-        dispatch(emptyCart())
-      }}
+      onPress={emptyCartAlert}
       style={ styles.cartIcon}>
       <Image source={cartItems.length > 0 ? require("../assets/CartWithItems.png") : require("../assets/CartEmpty.png")}/>
     </TouchableOpacity>
