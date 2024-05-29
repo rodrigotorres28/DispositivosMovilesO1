b9cc2ca88a80335806592abc38948c4b3b18f76a
Rodrigo Torres <32571199+rodrigotorres28@users.noreply.github.com>
2024-05-29 02:27:29 -0300

    useFetchProducts con formateo

--- customHooks/useFetchProducts.ts
+++ customHooks/useFetchProducts.ts
@@ -1,10 +1,46 @@
+// customHooks/useFetchProducts.ts
+
 import { useState, useEffect } from "react";
 
 import productEndpoints from "../axios/endpoints";
 import { Product } from "../types/Product";
 
+//ProductsCAtegory is compatible with the SectionList in ProductSectionList
+interface ProductsCategory {
+  id: number;
+  categoryName: string;
+  data: Product[];
+}
+
+//Format the api response into ProductsCategory
+const formatProducts = (products: Product[]): ProductsCategory[] => {
+  const categoriesMap: { [key: string]: Product[] } = {};
+
+  products.forEach((product) => {
+    if (!categoriesMap[product.category]) {
+      categoriesMap[product.category] = [];
+    }
+    categoriesMap[product.category].push(product);
+  });
+
+  const categoriesArray: ProductsCategory[] = [];
+
+  const categoryNames = Object.keys(categoriesMap);
+
+  categoryNames.forEach((categoryName, index) => {
+    const category: ProductsCategory = {
+      id: index,
+      categoryName,
+      data: categoriesMap[categoryName],
+    };
+    categoriesArray.push(category);
+  });
+
+  return categoriesArray;
+};
+
 const useFetchProducts = () => {
-  const [data, setData] = useState<Product[] | null>(null);
+  const [data, setData] = useState<ProductsCategory[] | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -13,7 +49,8 @@ const useFetchProducts = () => {
       setLoading(true);
       try {
         const products = await productEndpoints.fetchProducts();
-        setData(products);
+        const formattedData = formatProducts(products);
+        setData(formattedData);
         setError(null);
       } catch (error) {
         setError(error as Error);
